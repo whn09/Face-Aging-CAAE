026d6d8677b33430971203457d9d1e7c70afdc45
henan.wang <henan.wang@henan.wang's MacBook Pro>
2019-01-14 15:33:02 +0800

    add script

---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python main.py --is_train False --testdir ../algo_recommend_server/script/Face/face_model/faces/test/ --savedir save
---
+++ train.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python main.py --dataset UTKFace --savedir save --epoch 3 --use_trained_model True --use_init_model	True
