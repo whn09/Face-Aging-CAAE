402523dd2cf9f4ea063457deaa78bbd207ddefa4
henan.wang <henan.wang@henan.wang's MacBook Pro>
2019-01-14 19:03:46 +0800

    change test

--- train.sh
+++ train.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-python main.py --dataset UTKFace --savedir save --epoch 10 --use_trained_model True --use_init_model	True
+#python main.py --dataset UTKFace --savedir save --epoch 10 --use_trained_model True --use_init_model True
+python main.py --dataset CACD --savedir save --epoch 2 --use_trained_model True --use_init_model True
