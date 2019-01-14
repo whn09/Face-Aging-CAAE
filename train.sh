bd27cd5c87877f614b222ded67c2abdaecac555e
henan.wang <henan.wang@henan.wang's MacBook Pro>
2019-01-14 15:33:55 +0800

    change n_epoch to 10

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python main.py --dataset UTKFace --savedir save --epoch 3 --use_trained_model True --use_init_model	True
+python main.py --dataset UTKFace --savedir save --epoch 10 --use_trained_model True --use_init_model	True
